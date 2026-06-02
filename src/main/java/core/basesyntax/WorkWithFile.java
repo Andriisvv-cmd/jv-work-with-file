a44ed6e9ebddb8611aee9097a528aa2c4f004613
Andrii <andriisopovskyi@gmail.com>
2026-06-02 10:46:15 +0200

    add filewriter

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class WorkWithFile {
