4678f99a2ed8ee9587b33c29793fd24fce85e309
Andrii <andriisopovskyi@gmail.com>
2026-06-02 10:37:18 +0200

    finish

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,42 @@
 package core.basesyntax;
 
+import java.io.*;
+
 public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
+        int supplySum = 0;
+        int buySum = 0;
 
+        try (BufferedReader reader = new BufferedReader(new FileReader(fromFileName))) {
+            String value = reader.readLine();
+            while (value != null) {
+                if (!value.isEmpty()) {
+                    String[] parts = value.split(",");
+                    String operation = parts[0];
+                    int amount = Integer.parseInt(parts[1]);
+                    if (operation.equals("supply")) {
+                        supplySum += amount;
+                    } else {
+                        buySum += amount;
+                    }
+                }
+                value = reader.readLine();
+            }
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName))) {
+                writer.write("supply," + supplySum);
+                writer.newLine();
+                writer.write("buy," + buySum);
+                writer.newLine();
+                writer.write("result," + (supplySum - buySum));
+
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write in the file ", e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file ", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't read file ", e);
+        }
     }
 }
